d041e41afa484af4157a60ebc0c15ff8fe25ac6c
若海 <wang@rehiy.com>
2021-12-20 10:32:23 +0800

    优化子账号支持

--- source/common.php
+++ source/common.php
@@ -16,13 +16,12 @@ if (empty($_COOKIE['cf_email']) || empty($_COOKIE['cf_api_key'])) {
     $action = $_GET['action'] = 'login';
 }
 
+$cf_email = $_COOKIE['cf_email'] ?? $_POST['cf_email'] ?? '';
+$cf_api_key = $_COOKIE['cf_api_key'] ??  $_POST['cf_api_key'] ?? '';
+
 if (is_file(__DIR__ . '/vaccount/caller.php')) {
     require_once __DIR__ . '/vaccount/caller.php';
 }
 
-$key = new \Cloudflare\API\Auth\APIKey(
-    $_COOKIE['cf_email'] ?? $_POST['cf_email'] ?? '',
-    $_COOKIE['cf_api_key'] ??  $_POST['cf_api_key'] ?? ''
-);
-
+$key = new \Cloudflare\API\Auth\APIKey($cf_email, $cf_api_key);
 $adapter = new Cloudflare\API\Adapter\Guzzle($key);
